469de76225ce7efa79207be1dd8567f5b687902b
dongyun <dongyun_1990@163.com>
2018-07-23 21:04:59 +0800

    sql初始化

--- src/main/resources/springboot.sql
+++ src/main/resources/springboot.sql
@@ -1,36 +1,15 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50560
- Source Host           : localhost:3306
- Source Schema         : springboot
-
- Target Server Type    : MySQL
- Target Server Version : 50560
- File Encoding         : 65001
-
- Date: 23/07/2018 14:20:32
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for spring_boot_test
--- ----------------------------
+DROP DATABASE IF EXISTS `springboot`;
+CREATE DATABASE `springboot`;
 DROP TABLE IF EXISTS `spring_boot_test`;
+USE `springboot`;
 CREATE TABLE `spring_boot_test`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
 
--- ----------------------------
--- Records of spring_boot_test
--- ----------------------------
-INSERT INTO `spring_boot_test` VALUES (1, '李刚刚');
-INSERT INTO `spring_boot_test` VALUES (2, '赵蛋蛋');
+INSERT INTO `spring_boot_test`
+VALUES (1, '李刚刚');
 
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `spring_boot_test`
+VALUES (2, '赵蛋蛋');
