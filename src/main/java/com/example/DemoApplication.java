852fac790cb88930b22cdd518fd98b1d07658980
dongyun90 <dy22135247>
2018-07-26 16:35:26 +0800

    springboot集成springcloud,配置eureka server和client

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,11 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "**.mapper")
 @EnableCaching
-
+//@EnableEurekaServer
+//@EnableEurekaClient
 public class DemoApplication {
 
     public static void main(String[] args) {
